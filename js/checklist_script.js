dfcd563f736caae79b0d7ad32eb5416bd0374163
Jamin <jamind2022@gmail.com>
2026-04-03 20:13:27 -0400

    Storing grocery lists in local storage, fixed a tab color issue

--- js/checklist_script.js
+++ js/checklist_script.js
@@ -1,13 +1,18 @@
-let pairList = {
-    "My List" : [["Apples", 1, "count", false], ["Siracha Sauce", 8, "fl. oz.", false], ["Flour", 2, "lbs.", false]],
-    "Spring Harvest" : [["Apples", 1, "count", false], ["Juice", 2, "fl. oz.", false],],
-
-};
-
-
+let lists = getGroceryLists();
+// Create the pre-populated default list if it doesn't exist yet
+if (!("My List" in lists)) {
+    lists["My List"] = [["Apples", 1, "count", false], ["Siracha Sauce", 8, "fl. oz.", false], ["Flour", 2, "lbs.", false]];
+    localStorage.setItem('groceryLists', JSON.stringify(lists));
+}
 
 let currList = "My List";
-reDisplay();
+
+/** Always returns a dictionary (handles legacy localStorage that stored one object instead of an array). */
+function getGroceryLists() {
+    storedData = localStorage.getItem('groceryLists');
+    pairList = storedData ? JSON.parse(storedData) : {};
+    return pairList;
+}
 
 function addItemFromUserInput() {
     const text = document.getElementById("item-name-input").value;
@@ -18,24 +23,27 @@ function addItemFromUserInput() {
         return;
     }
 
-    addItem(currList,text, qty, units);
+    addItem(currList, text, qty, units);
     reDisplay();
 
     document.getElementById("item-name-input").value = "";
     document.getElementById("item-qty-input").value = "";
     document.getElementById("item-units-input").value = "";
-    
+
 }
 
-function addItem(listname, text, qty, units){
+function addItem(listname, text, qty, units) {
+    pairList = getGroceryLists();
     pairList[listname].push([text, qty, units, false]);
     console.log(`added: ${text} ${qty} ${units}`)
+    localStorage.setItem('groceryLists', JSON.stringify(pairList));
 }
 
 function reDisplay() {
     const itemsDiv = document.getElementById("items");
     itemsNewHtml = "";
 
+    pairList = getGroceryLists();
     pairList[currList].forEach(function (pair) {
         text = pair[0];
         qty = pair[1];
@@ -55,72 +63,13 @@ function reDisplay() {
     itemsDiv.innerHTML = itemsNewHtml;
 }
 
-function deleteItem(button) {
-    const textToDelete = button.parentElement.querySelector("span").textContent;
-    pairList[currList] = pairList[currList].filter(pair => pair[0] + " (" + pair[1] + " " + pair[2] + ")" !== textToDelete);
-    reDisplay();
-}
-function setCheckTrue(checkbox) {
-    const textToKeepChecked = checkbox.parentElement.querySelector("span").textContent;
-    console.log(`checked: ${textToKeepChecked}`)
-    pairList[currList].forEach(tuple => {
-        if (tuple[0] + " (" + tuple[1] + " " + tuple[2] + ")" === textToKeepChecked) {
-            tuple[3] = !tuple[3];
-        }
-    });
-}
-
-function switchList(listname){
-    currList = listname;
-    document.getElementById("list-title").innerHTML = listname;
-    reDisplay();
-}
-
-// Button code based off of https://www.w3schools.com/howto/howto_js_tab_header.asp
-
-function openTab(tabname, elmnt, marker) {
-
-  // Remove the background color of all tablinks/buttons
-  tablinks = document.getElementsByClassName("listlink");
-  marks = document.getElementsByClassName("current-tab-marker");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.color = "";
-    marks[i].style.backgroundColor = "";
-  }
-
-  // Change the font colors and the color of the bar at the bottom 
-  marker = document.getElementById(marker);
-  elmnt.style.color = "#342066ff";
-  marker.style.backgroundColor = "blue";
-
-  switchList(tabname);
-}
-
-// When the user clicks on the button, open the modal
-function newListMenu(){
-    // Change the font colors and the color of the bar at the bottom 
-    marker = document.getElementById("new-list-marker");
-    text = document.getElementById("newlist-button");
-    text.style.color = "lightblue";
-    marker.style.backgroundColor = "lightblue";
-
-    modal = document.getElementById("new-list-menu");
-    modal.style.display = "block";
-}
-
-function createNewList(){
-    const text = document.getElementById("list-name-input").value;
-
-    if (!text) {
-        return;
-    }
-    pairList[text] = [];
-
+function updateListLinks() {
+    pairList = getGroceryLists();
     const listTabsDiv = document.getElementsByClassName("list-tabs")[0];
     listTabsNewHTML = "";
 
     Object.keys(pairList).forEach(key =>
-        listTabsNewHTML += `<button class="listlink" onclick="openTab('${key}', this, '${key}-marker')">
+        listTabsNewHTML += `<button id='${key}-button' class="listlink" onclick="openTab('${key}', '${key}-marker')">
                 <span class="tab-label">${key}</span>
                 <div id='${key}-marker' class="current-tab-marker"></div>
             </button>`
@@ -132,15 +81,88 @@ function createNewList(){
             </button>`
 
     listTabsDiv.innerHTML = listTabsNewHTML;
+}
+
+function deleteItem(button) {
+    const textToDelete = button.parentElement.querySelector("span").textContent;
+    pairList = getGroceryLists();
+    pairList[currList] = pairList[currList].filter(pair => pair[0] + " (" + pair[1] + " " + pair[2] + ")" !== textToDelete);
+    localStorage.setItem('groceryLists', JSON.stringify(pairList));
+    reDisplay();
+}
+function setCheckTrue(checkbox) {
+    pairList = getGroceryLists();
+    const textToKeepChecked = checkbox.parentElement.querySelector("span").textContent;
+    console.log(`checked: ${textToKeepChecked}`)
+    pairList[currList].forEach(tuple => {
+        if (tuple[0] + " (" + tuple[1] + " " + tuple[2] + ")" === textToKeepChecked) {
+            tuple[3] = !tuple[3];
+        }
+    });
+    localStorage.setItem('groceryLists', JSON.stringify(pairList));
+}
+
+// Button code based off of https://www.w3schools.com/howto/howto_js_tab_header.asp
+
+function openTab(tabname, marker) {
+    currList = tabname;
+    document.getElementById("list-title").innerHTML = tabname;
+    
+    reDisplay();
+    updateListLinks();
+
+    // Remove the background color of all tablinks/buttons
+    tablinks = document.getElementsByClassName("listlink");
+    marks = document.getElementsByClassName("current-tab-marker");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.color = "";
+        marks[i].style.backgroundColor = "";
+        console.log("text and marker default colored")
+    }
+
+    // Change the font colors and the color of the bar at the bottom 
+    marker = document.getElementById(marker);
+    elmnt = document.getElementById(`${tabname}-button`);
+    console.log(elmnt);
+    elmnt.style.color = "#342066ff";
+    console.log(elmnt.style)
+    marker.style.backgroundColor = "blue";
+}
+
+// When the user clicks on the button, open the modal
+function newListMenu() {
+    // Change the font colors and the color of the bar at the bottom 
+    marker = document.getElementById("new-list-marker");
+    text = document.getElementById("newlist-button");
+    text.style.color = "lightblue";
+    marker.style.backgroundColor = "lightblue";
+
+    modal = document.getElementById("new-list-menu");
+    modal.style.display = "block";
+}
+
+function createNewList() {
+    const text = document.getElementById("list-name-input").value;
+
+    if (!text) {
+        return;
+    }
+    pairList = getGroceryLists();
+    pairList[text] = [];
+    localStorage.setItem('groceryLists', JSON.stringify(pairList));
+    console.log(getGroceryLists());
+
     currList = text;
     reDisplay();
+    updateListLinks();
     document.getElementById("list-name-input").value = "";
     console.log(`added new list: ${text}`);
 
     closeNewListMenu();
 
     alltabs = document.getElementsByClassName("listlink");
-    openTab(text, alltabs[alltabs.length -2], `${text}-marker`);
+    openTab(text, alltabs[alltabs.length - 2], `${text}-marker`);
+
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -154,12 +176,14 @@ function closeNewListMenu() {
     modal.style.display = "none";
 }
 
-window.onclick = function(event){
+window.onclick = function (event) {
     modal = document.getElementById("new-list-menu");
     if (event.target == modal) {
         closeNewListMenu();
     }
 };
 
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click(); 
+reDisplay();
+updateListLinks();
+// Get the element with id="defaultList" and click on it
+openTab("My List", "My List-marker");
